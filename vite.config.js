51964595240677fb7043e5eb240d393db8e6ee09
Ajith Anand <ajithanand303@gmail.com>
2023-09-15 22:21:22 +0530

    config change: base path set dynamic

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,28 @@
-import { defineConfig,loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const basename = mode === 'production' ? '/Olx-Clone/' : '/';
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd());
   return {
     define: {
-      'process.env': env
+      'process.env': env,
     },
-    base: "/Olx-Clone/",
+    base: basename,
     plugins: [react()],
     build: {
-      // chunkSizeWarningLimit: 1600,
       rollupOptions: {
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
             }
-        }
-    }
-    },  
-  }
-})
+          },
+        },
+      },
+    },
+  };
+});
