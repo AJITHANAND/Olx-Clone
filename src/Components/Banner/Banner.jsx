7a7f69a27ba0afd47c6495e449ec90c9c4610a15
Ajith Anand <ajithanand303@gmail.com>
2023-09-16 22:35:08 +0530

    Mobile : fit #2

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -3,6 +3,7 @@ import "./Banner.css";
 import Arrow from "../../assets/Arrow";
 import { useContext } from "react";
 import { SearchContext } from "../../Contexts/SearchContext";
+import { categoriesContent } from "../../Constants/categories";
 function Banner() {
   const { setSearch } = useContext(SearchContext);
 
@@ -11,35 +12,65 @@ function Banner() {
   return (
     <div className="bannerParentDiv">
       <div className="bannerChildDiv">
-        <div className="menuBar d-flex align-items-center justify-content-center">
-          <div className="categoryMenu">
-            <span>ALL CATEGORIES</span>
-            {/* <Arrow></Arrow>  */}
+        {isMobile ? (
+          <div className="menuBar">
+            <div className="mobileBanner">
+              <div className="mobileBannerHeading">
+                <p>Browse Categories</p>
+              </div>
+              <div className="mobileBannerItems">
+                {categoriesContent.map((category, index) => (
+                  <div 
+                  className="itemBox" 
+                  key={index}
+                  onClick={() => setSearch(category.name)}
+                  >
+                    <a >
+                      <picture>
+                        <img src={category.mobileIcon} alt="icon" />
+                      </picture>
+                    </a>
+                    <p>{category.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="otherQuickOptions">
-            <span onClick={(e) => setSearch(e.target.innerText)}>Cars</span>
-            <span onClick={(e) => setSearch("Bikes")}>Motorcycles</span>
-            <span onClick={(e) => setSearch("Mobiles")}>Mobile Phones</span>
-            <span onClick={(e) => setSearch("For Sale: Houses & Apartments")}>
-              For Sale: Houses & Apartments
-            </span>
-            <span onClick={(e) => setSearch("Bikes")}>Scooters</span>
-            <span onClick={(e) => setSearch("Commercial & Other Vehicles")}>
-              Commercial & Other Vehicles
-            </span>
-            <span onClick={(e) => setSearch("For Rent: Houses & Apartments")}>
-              For Rent: Houses & Apartments
-            </span>
-          </div>
-        </div>
-        {!isMobile && (
-          <div className="banner">
-            <img
-              // src="../../../Images/banner copy.png"
-              src="https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/images/banner.jpg"
-              alt=""
-            />
-          </div>
+        ) : (
+          <>
+            <div className="menuBar d-flex align-items-center justify-content-center">
+              <div className="categoryMenu">
+                <span>ALL CATEGORIES</span>
+                {/* <Arrow></Arrow>  */}
+              </div>
+              <div className="otherQuickOptions">
+                <span onClick={(e) => setSearch(e.target.innerText)}>Cars</span>
+                <span onClick={(e) => setSearch("Bikes")}>Motorcycles</span>
+                <span onClick={(e) => setSearch("Mobiles")}>Mobile Phones</span>
+                <span
+                  onClick={(e) => setSearch("For Sale: Houses & Apartments")}
+                >
+                  For Sale: Houses & Apartments
+                </span>
+                <span onClick={(e) => setSearch("Bikes")}>Scooters</span>
+                <span onClick={(e) => setSearch("Commercial & Other Vehicles")}>
+                  Commercial & Other Vehicles
+                </span>
+                <span
+                  onClick={(e) => setSearch("For Rent: Houses & Apartments")}
+                >
+                  For Rent: Houses & Apartments
+                </span>
+              </div>
+            </div>
+            <div className="banner">
+              <img
+                // src="../../../Images/banner copy.png"
+                src="https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/images/banner.jpg"
+                alt=""
+              />
+            </div>
+          </>
         )}
       </div>
     </div>
--- src/Constants/categories.jsx
+++ src/Constants/categories.jsx
@@ -13,46 +13,55 @@ export const categoriesContent = [
     name: "Cars",
     icon: <CarIcon />,
     brand: ["Toyota", "Honda", "Ford", "BMW", "Audi", "Tata", "Maruti-Suzuki"],
+    mobileIcon :"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/cars.png"
   },
   {
     name:'Properties',
     icon: <HouseIcon />,
-    type:["For Sale: Houses & Apartments","For Rent: Houses & Apartments","Land","PG","Guest House"]
+    type:["For Sale: Houses & Apartments","For Rent: Houses & Apartments","Land","PG","Guest House"],
+    mobileIcon :"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/properties.png"
   },
   {
     name: "Mobiles",
     icon: <MobileIcon />,
     brand: ["Samsung", "Apple", "OnePlus", "Oppo", "Vivo", "Realme", "Mi"],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/mobile.png"
   },
   {
     name: "Bikes",
     icon: <BikeIcon />,
     brand: ["Bajaj", "Yamaha", "KTM",'Royal Enfield','Honda', 'Hero','TVS', 'Suzuki', 'BMW','Ather'],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/bike.png"
   },
   {
     name: "Electronics & Appliances",
     icon: <ElectronicIcon />,
+    mobileIcon: "https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/electronics.png"
   },
   {
     name: "Furniture",
     icon: <FurnitureIcon />,
-    type:['Tables', 'Chairs', 'Couches', 'Beds' ]
+    type:['Tables', 'Chairs', 'Couches', 'Beds' ],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/furniture.png"
   },
   {
     name: "Fashion",
     icon: <FashionIcon />,
     type: ["Men", "Women", "Kids"],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/fashion.png"
 
   },
   {
     name: "Pets",
     icon: <PetsIcon />,
     type: ["Dog", "Cat", "Bird","Other"],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/pets.png"
   },
   {
     name: "Services",
     icon: <ServicesIcon />,
     type: ["Car Wash", "Car Repair", "Car Insurance", "Car Rental", "Car Cleaning"],
+    mobileIcon:"https://raw.githubusercontent.com/AJITHANAND/Olx-Clone/deploy/static/icons/service.png"
   }
   ,
 ];
