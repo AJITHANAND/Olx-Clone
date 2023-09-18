adb2c8364a2d9bd7f1a6a890228057dbe8f12c7a
Ajith Anand <ajithanand303@gmail.com>
2023-09-19 00:21:23 +0530

    google translate urls

---
+++ src/Constants/translate.jsx
@@ -0,0 +1,7 @@
+// google translate 
+// main : "https://ajithanand.github.io"
+export const url_main = "https://ajithanand.github.io/Olx-Clone/"
+export const url_host_path  = "https://ajithanand-github-io"
+export const url_base_path = "Olx-Clone"
+
+export const hindi_param = `hi`
